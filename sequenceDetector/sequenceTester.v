a61f6ca5d26755ffec10bd49b0dcc0ebe05857ab
Aditya Agarwal <76390138+tendo14@users.noreply.github.com>
2022-07-03 00:16:07 +0530

    Update sequenceTester.v

--- sequenceDetector/sequenceTester.v
+++ sequenceDetector/sequenceTester.v
@@ -1,1 +1,51 @@
+`timescale 1ns / 1ps
 
+module seqTester;
+
+reg sequence_in;
+reg reset;
+reg clock;
+
+wire detector_out;
+
+seqDetector one(
+.sequence_in(sequence_in),
+.clock(clock),
+.reset(reset),
+.detector_out(detector_out)
+);
+
+initial begin
+	clock = 0;
+	forever
+		#5 clock = ~clock;
+end
+
+initial begin
+	sequence_in = 0;
+	reset = 1;
+	#30
+	reset = 0;
+	#40;
+	sequence_in = 1;
+	#10;
+	sequence_in = 0;
+	#10;
+	sequence_in = 1;
+	#10;
+	sequence_in = 0;
+	#10;
+	sequence_in = 1;
+	#10;
+	sequence_in = 1;
+	#10;
+	sequence_in = 0;
+	#10;
+	sequence_in = 1;
+	#10;
+	sequence_in = 1;
+	#10;
+	sequence_in = 0;
+end
+
+endmodule
