b418766ccee4047fe0b232cd02b8a821d7c1a85b
Aditya Agarwal <76390138+tendo14@users.noreply.github.com>
2022-07-04 18:51:10 +0530

    Update fullAdderXor.v

--- adderSubtractor/fullAdderXor.v
+++ adderSubtractor/fullAdderXor.v
@@ -1,1 +1,17 @@
+module fullAdderXOR(
+input Cin,
+input A,
+input B,
+input M,
+output Cout,
+output S
+);
 
+wire I;
+
+assign I = M^B;
+
+assign S = Cin^(A^I);
+assign Cout = Cin*(A^I) + A*I;
+
+endmodule
