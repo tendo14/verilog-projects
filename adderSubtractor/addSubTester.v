10722d80b0d978c42bb942ffbd77f6c1c59d3b8f
Aditya Agarwal <76390138+tendo14@users.noreply.github.com>
2022-07-04 18:50:07 +0530

    Update addSubTester.v

--- adderSubtractor/addSubTester.v
+++ adderSubtractor/addSubTester.v
@@ -1,1 +1,39 @@
+`timescale 1ns/1ps
 
+module addSubTester;
+
+reg [7:0] A;
+reg [7:0] B;
+reg M;
+
+wire [7:0] C;
+wire Cout;
+
+eightBitAdderSubtractor uut
+(
+.A(A),
+.B(B),
+.M(M),
+.Cin(M),
+.S(C),
+.Cout(Cout)
+);
+
+initial
+begin
+	A = 8'd0;
+	B = 8'd0;
+	M = 0;
+	
+	#50;
+	
+	A = 8'h3A;
+	B = 8'h38;
+	M = 0;
+	
+	#50;
+	
+	M = 1;
+end
+
+endmodule
